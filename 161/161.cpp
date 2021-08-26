38696a2029c6ca5296aa0389e8274dee30b84850
Slturner980 <slturner980@gmail.com>
2021-08-25 19:34:59 -0500

    161

--- 161/161.cpp
+++ 161/161.cpp
@@ -8,12 +8,34 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <iostream>
+#include <fstream>
+#include <vector>
 
 using namespace std;
 
 int main()
-{
-    cout << "hello";
+{   vector<int> BaseArray;
+    vector<int> CountArray;
+    int i;
+    int j;
+
+    ifstream file;
+    file.open ("in1");
+
+    file >> j;
+
+    while (j=! 0){
+        for ( i = 0; i < 100; i++)
+        {
+            file >> BaseArray[i];
+        }
+        
+    }
+
+    for (i = 0; i < 100; i++)
+    {
+       cout << BaseArray[i] << endl;
+    }
 
     return 0;
 }
